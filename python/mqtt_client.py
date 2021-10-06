a529b92b61d6e8371dba2e33866f132d4c106ac0
Nudelsalat <fabian.dressler@me.com>
2021-10-07 00:29:37 +0200

    fixup! mqtt-client: add status request functionalities

--- python/mqtt_client.py
+++ python/mqtt_client.py
@@ -79,7 +79,7 @@ class mqtt_client(gr.basic_block):
 
         # Type = StatusRequest
         if decoded_message["Type"] == "StatusRequest":
-            self._handle_status_request(self, decoded_message["Message"])
+            self._handle_status_request(decoded_message["Message"])
 
 
         # Connector to GNURadio
